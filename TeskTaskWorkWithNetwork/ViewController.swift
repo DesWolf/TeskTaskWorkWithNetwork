b805cea367604103de3902979ad556d3fc259529
DesWolf <okuneev@gmail.com>
2020-02-19 13:30:24 +0700

    Initial Commit

---
+++ TeskTaskWorkWithNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeskTaskWorkWithNetwork
+//
+//  Created by Максим Окунеев on 2/19/20.
+//  Copyright © 2020 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
